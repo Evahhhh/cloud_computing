ae143b33075c95230ff502026fed581d8b4b30c4
Evah <eve-anneoffredi@orange.fr>
2026-06-09 15:51:59 +0200

    Projet 06 - Scalabilite

---
+++ 06_scalabilite/network.tf
@@ -0,0 +1,33 @@
+# === network.tf ===
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags       = { Name = "scaling-vpc" }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags              = { Name = "public-subnet" }
+}
+
+resource "aws_security_group" "app_sg" {
+  name   = "app-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP application"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
